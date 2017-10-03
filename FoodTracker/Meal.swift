dbd6a5520c55e682bf767a801a741113cb959e9c
Kevin <kevin98gray@gmail.com>
2017-10-03 21:31:46 +0800

    Implement ratings and add unit tests

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,36 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Kevin Chan on 03/10/2017.
+//  Copyright © 2017 Kevin Chan. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating should be between 0 and 5 exclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+    }
+}
