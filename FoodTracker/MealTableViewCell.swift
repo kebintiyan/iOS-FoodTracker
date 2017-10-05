1c0aeec6f95cdde79584283a4586c3c96c55e214
Kevin <kevin98gray@gmail.com>
2017-10-05 15:53:00 +0800

    Implement data model

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Kevin Chan on 05/10/2017.
+//  Copyright © 2017 Kevin Chan. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FoodTracker/MealTableViewController.swift
@@ -0,0 +1,124 @@
+//
+//  MealTableViewController.swift
+//  FoodTracker
+//
+//  Created by Kevin Chan on 05/10/2017.
+//  Copyright © 2017 Kevin Chan. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewController: UITableViewController {
+    
+    //MARK: Properties
+    var meals = [Meal]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Load the sample data.
+        loadSampleMeals()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "MealTableViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell")
+        }
+
+        // Fetches the appropriate meal for the data source layout.
+        let meal = meals[indexPath.row]
+
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+        
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: Private Methods
+    
+    private func loadSampleMeals() {
+        let photoPizza = UIImage(named: "pizza")
+        let photoBurger = UIImage(named: "burger")
+        let photoSalmon = UIImage(named: "salmon")
+        
+        guard let mealPizza = Meal(name: "Pizza", photo: photoPizza, rating: 3) else {
+            fatalError("Unable to instantiate pizza meal")
+        }
+        
+        guard let mealBurger = Meal(name: "Burger", photo: photoBurger, rating: 2) else {
+            fatalError("Unable to instantiate burger meal")
+        }
+        
+        guard let mealSalmon = Meal(name: "Salmon", photo: photoSalmon, rating: 5) else {
+            fatalError("Unable to instantiate salmon meal")
+        }
+        
+        meals += [mealPizza, mealBurger, mealSalmon]
+        
+    }
+    
+}
